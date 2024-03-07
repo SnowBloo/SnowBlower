1c872c617cd7d9a09d2a10f8d11093475d07ce4e
albert chen <chenalbert06@gmail.com>
2024-03-07 09:18:41 -0500

    determine focal length and main detection code

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 This is code for our snow blower product
 
 Made by team SnowBlo
+
+Top left = (0,0)
+As you move right it'll increase x value and as you move down it'll increase y value
+Need to determine camera resolution to figure out the actual bounds
+
+camera focal length: 1049.21 !!!!
---
+++ camera/determine_focal_length.py
@@ -0,0 +1,129 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import apriltag
+import math
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (1366, 768)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(1366, 768))
+
+options = apriltag.DetectorOptions(families='tag36h11',
+								 border=1,
+								 nthreads=4,
+								 quad_decimate=1.0,
+								 quad_blur=0.0,
+								 refine_edges=True,
+								 refine_decode=False,
+								 refine_pose=False,
+								 debug=False,
+								 quad_contours=True)
+
+detector = apriltag.Detector(options)
+
+def draw_tag(image, results):
+	for r in results:
+		(ptA, ptB, ptC, ptD) = r.corners
+		ptB = (int(ptB[0]), int(ptB[1]))
+		ptC = (int(ptC[0]), int(ptC[1]))
+		ptD = (int(ptD[0]), int(ptD[1]))
+		ptA = (int(ptA[0]), int(ptA[1]))
+		
+		cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+		cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+		cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+		cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+		
+		tagID = r.tag_id
+		(cX, cY) = (int(r.center[0]), int(r.center[1]))
+		cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+		cv2.putText(image, str(tagID), (cX, cY - 15),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		
+		tagFamily = r.tag_family.decode("utf-8")
+		cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+	
+def draw_center_box(image, centers):
+	if len(centers) != 4:
+		return 
+	cv2.line(image, centers[0], centers[1], (0, 0, 255), 2)
+	cv2.line(image, centers[1], centers[3], (0, 0, 255), 2)
+	cv2.line(image, centers[3], centers[2], (0, 0, 255), 2)
+	cv2.line(image, centers[2], centers[0], (0, 0, 255), 2)
+
+def middle(centers):
+	if len(centers) == 0:
+		return (0, 0)
+	# calculate the centerpoint of the centers
+	x = 0
+	y = 0
+	for c in centers:
+		x += c[0]
+		y += c[1]
+		
+	x = int(x / len(centers))
+	y = int(y / len(centers))
+	
+	return (x, y)
+
+def determine_orientation(results):
+	return results[0].tag_id == 0
+
+def image_processing(image, results):
+	centers = []
+	for r in results:
+		centers.append((int(r.center[0]), int(r.center[1])))
+		
+	# draw_center_box(image, centers)
+
+	center = middle(centers)
+	
+	cv2.circle(image, center, 5, (0, 0, 255), -1)
+
+	draw_tag(image, results)
+	
+	if len(centers) == 2:
+		if determine_orientation(results):
+			cv2.putText(image, "forward", (center[0], center[1] - 15),
+				cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+		else:
+			cv2.putText(image, "backward", (center[0], center[1] - 15),
+				cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+
+
+def line_length(results):
+	
+	(ptA, ptB, ptC, ptD) = results[0].corners
+	ptA = (ptA[0], ptA[1])
+	ptB = (ptB[0], ptB[1])
+
+	return math.dist(ptA, ptB)
+
+		
+# allow the camera to warmup
+time.sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+	image = frame.array
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	results = detector.detect(gray)
+	
+	image_processing(image, results)
+
+	cv2.imshow("Image", image)
+	key = cv2.waitKey(1) & 0xFF
+	
+	rawCapture.truncate(0)
+
+	if key == ord("q"):
+		break
+	if key == ord("c"):
+		distance = str(input("input distance: "))
+		cv2.imwrite("distance_" + distance + " ft_" + "length_" + str(line_length(results)) + " px.jpg" , image)
+	
